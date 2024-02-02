9efa7deb6f8eebf657aa4bbe6e95fe65bc332c79
kiên phạm <fbkienpt@gmail.com>
2024-02-02 19:59:02 +0700

    add test

--- base-solidity/test.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.3;
-
-contract Test{
-    uint x;
-    uint public y;
-}
---
+++ base-test/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef double db;
+using namespace std;
+//JohnBelus//
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+ 
+    }
+}
